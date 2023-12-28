3b14fb04cdb589a70a7935ada99af252fcbc72d4
Harsh-git1 <130690167+Harsh-git1@users.noreply.github.com>
2023-12-28 13:28:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,22 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
-};
+	let j=0;
+	let ans = [];
 
+	while(j < arr.length) {
+		let sum = 0;
+		let tempar = [];
+		while(j < arr.length && sum + arr[j] <= n) {
+			sum += arr[j];
+			tempar.push(arr[j]);
+			j++;
+		}
+		ans.push(tempar);		
+	}
+	
+	return ans;
+};
+ 
 const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));
